f5388926be4da738e98f4fa7dd8c5d9dec5e58f8
Kagethelephant <kagethelephant@gmail.com>
2024-11-29 10:43:06 -0700

    Change permissions for polybar launch.sh so it can launch aumatically

--- nvim/.config/nvim/lua/kage/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/kage/plugins/colorscheme.lua
@@ -1,16 +1,8 @@
 return {
-  "sho-87/kanagawa-paper.nvim", -- we can load more than one theme here to have options
-  priority = 1000, -- Loads before anything
+  "sho-87/kanagawa-paper.nvim",
+  priority = 1000,
   config = function()
-    -- -- This is how we change the colors (idk where to get the naming convention)
-    -- local bg_color = #FFFFFF
-    -- require("kanagawa-paper").setup({
-    --   style = "style",
-    --   on_colors = function(colors)
-    --      color.bg_color = #FFFFFF
-    --   end
-    -- })
-    vim.o.background = "dark" -- Set to the dark version of the theme
-    vim.cmd.colorscheme "kanagawa-paper" -- Set the color scheme in vim
+    vim.o.background = "dark"
+    vim.cmd.colorscheme "kanagawa-paper"
   end
 }
